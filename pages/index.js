b2eddc1d966b871e314fceb76ea63c8b19a1d441
naouichia <naim.aouaichia@ext.ingroupe.com>
2023-07-19 23:21:18 +0200

    Need count activation and remove link with my name to calendly. after can launch facebook ads

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,61 @@
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
-
+import moment from 'moment';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Layout, { GradientBackground } from '../components/Layout';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
+import React, { useState, useEffect } from 'react'
 
 export default function Index({ posts, globalData }) {
+
+    const [day, setDay] = useState(0);
+    const [hour, setHour] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [seconde, setSeconde] = useState(0);
+
+
+    useEffect(() => {
+        console.log('tototo')
+
+        return () => {
+            false;
+        }
+    })
   return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
+
+          <div className="main">
+              <div id='content'>
+                  <div className='title'>
+                      <span>CYBER WITH LOVE</span>
+                  </div>
+                  <br/>
+                  <p>école de cyber sécurité</p>
+                  <br/>
+                  <p style={{ fontSize: "14px" }}>Limite avant inscription</p>
+                  <section>
+                      <ul id="countdown">
+                          <li><span className="days timenumbers">{day}</span>
+                              <p className="timeRefDays timedescription">days</p>
+                          </li>
+                          <li><span className="hours timenumbers">{hour}</span>
+                              <p className="timeRefHours timedescription">hours</p>
+                          </li>
+                          <li><span className="minutes timenumbers">{minute}</span>
+                              <p className="timeRefMinutes timedescription">minutes</p>
+                          </li>
+                          <li><span className="seconds timenumbers yellow-text">{seconde}</span>
+                              <p className="timeRefSeconds timedescription">seconds</p>
+                          </li>
+                      </ul>
+                  </section>
+                  <p>Candidater <a href="https://calendly.com/naim-aouaichia/30min" target="_blank">et optenez votre place pour la prochaine rentré</a></p>
+
+              </div>
+          </div>
+
   );
 }
 
